3019c6f40c0494f0390e394d7a68b7d07ff330dc
Ilya Amelevich <ilya.amelevich@gmail.com>
2022-12-31 01:08:45 +0100

    Init

---
+++ OrderBookEntry.cpp
@@ -0,0 +1,33 @@
+#include "OrderBookEntry.h"
+
+#include <utility>
+
+OrderBookEntry::OrderBookEntry( double _price, 
+                        double _amount, 
+                        std::string _timestamp, 
+                        std::string _product, 
+                        OrderBookType _orderType, 
+                        std::string _username)
+: price(_price), 
+  amount(_amount), 
+  timestamp(std::move(_timestamp)),
+  product(std::move(_product)),
+  orderType(_orderType), 
+  username(std::move(_username))
+{
+  
+    
+}
+
+OrderBookType OrderBookEntry::stringToOrderBookType(const std::string& s)
+{
+  if (s == "ask")
+  {
+    return OrderBookType::ask;
+  }
+  if (s == "bid")
+  {
+    return OrderBookType::bid;
+  }
+  return OrderBookType::unknown;
+}
